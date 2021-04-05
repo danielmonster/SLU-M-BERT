f1678cd588de0fe3245fdbe0f2d462214caf46d3
danielmonster <danielguo123456@hotmail.com>
2021-04-05 06:43:44 +0000

    modify data split in enfr

--- preprocess/preprocess_enfr.py
+++ preprocess/preprocess_enfr.py
@@ -7,13 +7,13 @@ import json
 import argparse
 
 
-# python3 preprocess_enfr.pt --lang=en
-# python3 preprocess_enfr.pt --lang=fr
+# python3 preprocess_enfr.py --lang=en
+# python3 preprocess_enfr.py --lang=fr
 
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--split_ratio', type=float, default=0.15)
+    parser.add_argument('--split_ratio', type=float, default=0.25)
     parser.add_argument('--lang', type=str, required=True, help="Either 'en' (english) or 'fr' (french)")
     parser.add_argument('--memory_dir', type=str, default="../memory/enfr")
     return parser.parse_args()
@@ -42,10 +42,11 @@ def build_phone_vocab(df_all):
     return phone2idx
 
 
-def save_train_test(df_train, df_test, save_dir):
+def save_train_test(df_train, df_dev, df_test, save_dir):
     df_train.to_csv(os.path.join(save_dir, "train.csv"), encoding='utf-8', index=False)
+    df_dev.to_csv(os.path.join(save_dir, "dev.csv"), encoding='utf-8', index=False)
     df_test.to_csv(os.path.join(save_dir, "test.csv"), encoding='utf-8', index=False)
-    print("train.csv and test.csv saved in {}".format(os.path.abspath(save_dir)))
+    print("train.csv, dev.csv, and valid.csv saved in {}".format(os.path.abspath(save_dir)))
 
 
 def save_phone_idx(phone2idx, save_dir):
@@ -77,6 +78,7 @@ def get_xy(df, phone2idx):
 def save_numpy_data(data, filename):
     with open(filename, 'wb') as f:
         np.save(f, data)
+    print("Saved in {}".format(filename))
 
 
 
@@ -90,17 +92,29 @@ def main():
 
     # Train-test split with ratio = 0.15
     ratio = args.split_ratio
-    train_en, test_en = train_test_split(df_en_all, random_state=1, test_size=ratio, shuffle=True)
+    train_en, valid_en = train_test_split(df_en_all, random_state=1, test_size=ratio, shuffle=True)
+    dev_en, test_en = train_test_split(valid_en, random_state=1, test_size=0.5)
     # Reset indices of dataframe
     train_en = train_en.reset_index(drop=True)
+    dev_en = dev_en.reset_index(drop=True)
     test_en = test_en.reset_index(drop=True)
     # Save train and test files in "memory" directory
-    save_train_test(train_en, test_en, memory_path)
+    save_train_test(train_en, dev_en, test_en, memory_path)
 
     # Save x and y
     train_x, train_labels = get_xy(train_en, phone2idx)
     save_numpy_data(train_x, os.path.join(memory_path, "train_x.npy"))
     save_numpy_data(train_labels, os.path.join(memory_path, "train_y.npy"))
+
+    dev_x, dev_labels = get_xy(dev_en, phone2idx)
+    save_numpy_data(dev_x, os.path.join(memory_path, "dev_x.npy"))
+    save_numpy_data(dev_labels, os.path.join(memory_path, "dev_y.npy"))
+
+    test_x, test_labels = get_xy(test_en, phone2idx)
+    save_numpy_data(test_x, os.path.join(memory_path, "test_x.npy"))
+    save_numpy_data(test_labels, os.path.join(memory_path, "test_y.npy"))
+
+
 
 
 if __name__ == '__main__':
